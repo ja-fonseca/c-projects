f499260833ff18b0c4a4e1b16779576a9e0e1a52
Jose A. Fonseca <jose.fonseca@cea.fr>
2020-07-31 15:54:52 +0200

    add fprintf vector and matrix

--- vector-and-matrix/array.h
+++ vector-and-matrix/array.h
@@ -37,6 +37,15 @@
   putchar('\n');                                    \
 }while (0)
 
+#define fprint_vector(file_ptr, fmt, v, n) do{                  \
+  size_t              print_vector_loop_counter;                \
+  for (print_vector_loop_counter = 0;                           \
+       print_vector_loop_counter < (n);                         \
+       print_vector_loop_counter++)                             \
+    fprintf (file_ptr, fmt, (v)[print_vector_loop_counter]);    \
+  fprintf (file_ptr,"\n");                                     \
+}while (0)
+
 #define print_matrix(fmt, v, m, n) do{                      \
   size_t              print_matrix_loop_counter;            \
   for (print_matrix_loop_counter = 0;                       \
@@ -44,5 +53,14 @@
        print_matrix_loop_counter++)                         \
     print_vector (fmt, (v)[print_matrix_loop_counter], (n));\
   putchar('\n');                                            \
+}while (0)
+
+#define fprint_matrix(file_ptr, fmt, v, m, n) do{                       \
+  size_t              print_matrix_loop_counter;                        \
+  for (print_matrix_loop_counter = 0;                                   \
+       print_matrix_loop_counter < (m);                                 \
+       print_matrix_loop_counter++)                                     \
+    fprint_vector (file_ptr, fmt, (v)[print_matrix_loop_counter], (n)); \
+  fprintf (file_ptr,"\n");                                              \
 }while (0)
 #endif /* H_ARRAY_H */
